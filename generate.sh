b2641f5b8871f84b56971166c46b3fb2e0089825
zeta <zeta.ben@gmail.com>
2007-09-17 13:47:40 +0000

    Ajout du logo sur les thumbs

--- generate.sh
+++ generate.sh
@@ -4,4 +4,4 @@ seed=`date +%N`
 echo "seed is " $seed  "saving in map_"$seed".map";
 ./tremgen $seed > "map_"$seed".map";
 ./minimap $seed
-
+composite -gravity center -watermark 20 logo_labs.png levelshot.tga levelshot.tga
