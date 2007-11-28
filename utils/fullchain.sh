ff40cc201eaf904fde15c9eed8b974fdb86625c4
zeta <zeta.ben@gmail.com>
2007-11-28 20:03:09 +0000

    Fullchain V3 avec point d'exit et ligne finale

--- utils/fullchain.sh
+++ utils/fullchain.sh
@@ -3,7 +3,7 @@
 ##Generate.sh
 seed=`date +%N`
 echo "seed is " $seed  "saving in map_"$seed".map";
-./tremgen $seed > "map_"$seed".map"; 
+./tremgen $seed > "map_"$seed".map" || exit 1
 a="map_"$seed
 ##
 echo "working on "$a
@@ -19,12 +19,12 @@ f=`pwd`/$f;
 fi
 
 echo "Compiling "$f
-"/opt/gtkradiant/q3map2.x86" -v -game tremulous -fs_basepath "/usr/share/games/tremulous/" -fs_game base -custinfoparms -meta $f > "compile.txt"
-"/opt/gtkradiant/q3map2.x86" -v -game tremulous -fs_basepath "/usr/share/games/tremulous/" -fs_game base -vis -saveprt -fast $f >> "compile.txt"
-"/opt/gtkradiant/q3map2.x86" -v -game tremulous -fs_basepath "/usr/share/games/tremulous/" -fs_game base -light -fast  -super 2 -filter -custinfoparms $f  >> "compile.txt"
+"/opt/gtkradiant/q3map2.x86" -v -game tremulous -fs_basepath "/usr/share/games/tremulous/" -fs_game base -custinfoparms -meta $f > "compile.txt" || exit 2
+"/opt/gtkradiant/q3map2.x86" -v -game tremulous -fs_basepath "/usr/share/games/tremulous/" -fs_game base -vis -saveprt -fast $f >> "compile.txt" || exit 3
+"/opt/gtkradiant/q3map2.x86" -v -game tremulous -fs_basepath "/usr/share/games/tremulous/" -fs_game base -light -fast  -super 2 -filter -custinfoparms $f  >> "compile.txt" || exit 4
 ##
 
-tmp=`mktemp -td tremgen.XXXXXX` || exit 3
+tmp=`mktemp -td tremgen.XXXXXX` || exit 5
 echo "tempdir is "$tmp
 mkdir $tmp"/levelshots"
 mkdir $tmp"/maps"
@@ -42,3 +42,6 @@ cp $tmp/$a.pk3 .
 
 echo "cleaning up"
 rm -rf $tmp
+
+echo "saved as"
+echo $a.pk3
