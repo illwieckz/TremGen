81eb13fb469e1a88aef66f7dba918b8be25ed0f3
zeta <zeta.ben@gmail.com>
2007-11-28 19:35:32 +0000

    Scripts retravailles

--- generate.sh
+++ generate.sh
@@ -5,3 +5,4 @@ echo "seed is " $seed  "saving in map_"$seed".map";
 ./tremgen $seed > "map_"$seed".map";
 ./minimap $seed
 composite -gravity center -watermark 20 logo_labs.png levelshot.tga levelshot.tga
+echo "map_"$seed;
