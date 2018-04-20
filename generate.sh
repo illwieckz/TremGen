c85cca6b8c67e795db48c63c2e6e853b8c5008ed
Thomas Debesse <dev@illwieckz.net>
2018-04-20 02:11:39 +0200

    remove old generate/compil/test scripts

--- generate.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-seed=`date +%N`
-echo "seed is " $seed  "saving in map_"$seed".map";
-./tremgen $seed "map_"$seed".map";
-./minimap $seed
-composite -gravity center -watermark 20 logo_labs.png levelshot.tga levelshot.tga
-echo "map_"$seed;
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-f=$1
-mkdir -p ~/.tremulous/base/maps
-mkdir -p ~/.tremulous/base/levelshots
-cp $f* ~/.tremulous/base/maps/
-cp levelshot.tga ~/.tremulous/base/levelshots/$f.tga
-tremulous +sv_pure 0 +devmap $1
-
